3ccddc66603af7a4288aff15c62d1ec67a8b2560
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ActiveGroupNavigation/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.NavBar;
+
+namespace ActiveGroupNavigation
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        #region #CodeBehind
+        public Window1(){
+            InitializeComponent();
+            CheckButtonAvailability();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e){
+            int activegroupIndex = navBarControl1.Groups.IndexOf(navBarControl1.ActiveGroup);
+            if (sender == btnNext) navBarControl1.ActiveGroup = navBarControl1.Groups[activegroupIndex + 1];
+            if (sender == btnPrev) navBarControl1.ActiveGroup = navBarControl1.Groups[activegroupIndex - 1];
+        }
+
+        private void CheckButtonAvailability(){
+            btnNext.IsEnabled = !(navBarControl1.Groups.IndexOf(navBarControl1.ActiveGroup) == navBarControl1.Groups.Count - 1);
+            btnPrev.IsEnabled = !(navBarControl1.Groups.IndexOf(navBarControl1.ActiveGroup) == 0);
+        }
+
+        private void NavigationPaneView_ActiveGroupChanged(object sender, NavBarActiveGroupChangedEventArgs e){
+            CheckButtonAvailability();
+        }
+        #endregion #CodeBehind
+    }
+}
---
+++ VB/ActiveGroupNavigation/Window1.xaml.vb
@@ -0,0 +1,49 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.NavBar
+
+Namespace ActiveGroupNavigation
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		#Region "#CodeBehind"
+		Public Sub New()
+			InitializeComponent()
+			CheckButtonAvailability()
+		End Sub
+
+		Private Sub button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim activegroupIndex As Integer = navBarControl1.Groups.IndexOf(navBarControl1.ActiveGroup)
+			If sender Is btnNext Then
+				navBarControl1.ActiveGroup = navBarControl1.Groups(activegroupIndex + 1)
+			End If
+			If sender Is btnPrev Then
+				navBarControl1.ActiveGroup = navBarControl1.Groups(activegroupIndex - 1)
+			End If
+		End Sub
+
+		Private Sub CheckButtonAvailability()
+			btnNext.IsEnabled = Not(navBarControl1.Groups.IndexOf(navBarControl1.ActiveGroup) = navBarControl1.Groups.Count - 1)
+			btnPrev.IsEnabled = Not(navBarControl1.Groups.IndexOf(navBarControl1.ActiveGroup) = 0)
+		End Sub
+
+		Private Sub NavigationPaneView_ActiveGroupChanged(ByVal sender As Object, ByVal e As NavBarActiveGroupChangedEventArgs)
+			CheckButtonAvailability()
+		End Sub
+		#End Region ' #CodeBehind
+	End Class
+End Namespace
